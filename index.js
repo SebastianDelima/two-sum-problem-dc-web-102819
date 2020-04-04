c01981f5e7ab780aa52fb54be5f1474afc29c9f4
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-04-04 12:58:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-let arr = [5, -2, 4, 9, 1] 
-
-let sum = 6
 function bruteForceTwoSum(sumNum, array){
 
     let num1 = 0
-   
+    let arr = []
     let found = false
 
-    while (!found === true) {
+    while (num1 != array.length) {
 
     for(let counter = 1; counter < array.length; counter++)
 
     {
        if(array[num1] + array[counter] === sumNum){
         found = true
-         return [array[num1], array[counter]]
+      arr.push([array[num1], array[counter]])
        }
    
     }
     num1 += 1
-    if(num1 === array.length){
-        break
+    
     }
-    }
+   
+      let newArr = []
+      let counter = 1
+    arr.map((num) => {
+      if(arr[counter] !== undefined){
+      if (num[0] !== arr[counter][0] & num[1] !== arr[counter][1] ){
+        newArr.push(num)
+    console.log(counter)
+      }}
+    counter += 1
+   })
+    return newArr
 }
